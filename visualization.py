23ffd1a629f07d66f09180accbd3c4b8674e99b7
VitorMendonca62 <vitorqueiroz325@gmail.com>
2024-02-12 12:43:43 -0300

    tentando deixar bonito

--- visualization.py
+++ visualization.py
@@ -1,7 +1,8 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.Graph()
+seed = 20532
+G = nx.Graph(seed=seed)
 
 marked = []
 
@@ -18,13 +19,16 @@ def show_graph(nodes):
       
       distance = pointers[pointer_key]["distance"]
       G.add_edge(key, pointer_key, weight = distance)
-        
-  pos = nx.spring_layout(G)
+  
+  pos = nx.spring_layout(G, seed=seed) 
   edge_labels = nx.get_edge_attributes(G, 'weight')
-
-  nx.draw_networkx_nodes(G, pos, node_color='skyblue')
-  nx.draw_networkx_edges(G, pos, edge_color='k')
+  
+  nx.draw_networkx_nodes(G, pos, node_color = 'skyblue', node_size = 100)
+  nx.draw_networkx_edges(G, pos, edge_color = 'k')
   nx.draw_networkx_edge_labels(G, pos, edge_labels = edge_labels, font_size = 5 )
-  nx.draw_networkx_labels(G, pos, font_color='black', font_size= 8)
+  nx.draw_networkx_labels(G, pos, font_color = 'black', font_size = 6)
 
+  ax = plt.gca()
+  ax.margins(0)
+  plt.axis("off")
   plt.show()
