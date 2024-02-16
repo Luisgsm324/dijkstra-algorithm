538c8b932c244e4337df89b7b971d8ce33883cbf
VitorMendonca62 <vitorqueiroz325@gmail.com>
2024-02-16 12:29:52 -0300

    consertar erros de merging

--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
 from classes.Graph import Graph
 from take_data import take_stations, take_station
-<<<<<<< HEAD:src/main.py
 from dijkstra import dijkstra_algorithm
-=======
+
 from visualization import show_graph
->>>>>>> 23ffd1a629f07d66f09180accbd3c4b8674e99b7:main.py
 
 stations = take_stations()
 
@@ -13,12 +11,9 @@ graph = Graph()
 for key_station in stations:
   station = take_station(key_station)
   graph.add_vertice(key_station, station["connections"] )
-<<<<<<< HEAD:src/main.py
+
   
+show_graph(graph.nodes)
 dijkstra_algorithm("A13", "A01", graph)
 
 
-=======
-
-show_graph(graph.nodes)
->>>>>>> 23ffd1a629f07d66f09180accbd3c4b8674e99b7:main.py
