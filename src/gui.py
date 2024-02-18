0de58445087156268301333a593ee3eab63008d3
VitorMendonca62 <vitorqueiroz325@gmail.com>
2024-02-18 15:48:13 -0300

    consertando gui, posicao relativa dos textos

--- src/gui.py
+++ src/gui.py
@@ -8,6 +8,8 @@ class Screen:
         self.title = "Tokyo Metro"
         self.filephoto = 'assets\logo.png'
         self.window = Tk()
+        self.height = self.window.winfo_screenheight()
+        self.width = self.window.winfo_screenwidth()
         self.graph = graph
         self.initial_station_var, self.final_station_var = StringVar(), StringVar()
         self.result = None
@@ -22,6 +24,7 @@ class Screen:
         prev_frame.forget()
     
     def change_to_menuf(self): # Final -> Menu
+        self.label_result.destroy()
         self.menu_frame.pack(fill='both', expand=1)
         self.final_frame.forget()
     
@@ -39,13 +42,13 @@ class Screen:
     
     def run_menu_frame(self):
         start_button = Button(self.menu_frame, text='Algoritmo', bd=4, font=('Arial', 12), width=15, command=self.change_to_start)
-        start_button.place(x=570, y=300)
+        start_button.place(x = self.width // 2 - 150 // 2, y=300)
 
         visualize_button = Button(self.menu_frame, text='Visualizar', bd=4, font=('Arial', 12), width=15, command=self.run_graph_visualizer)
-        visualize_button.place(x=570, y=400)
+        visualize_button.place(x = self.width // 2 - 150 // 2, y=400)
         
         exit_button = Button(self.menu_frame, text='Sair', bd=4, font=('Arial', 12), width=15, command=self.window.destroy)
-        exit_button.place(x=570, y=500)
+        exit_button.place(x = self.width // 2 - 150 // 2, y=500)
 
         self.menu_frame.pack(fill='both', expand=1)
 
@@ -65,11 +68,10 @@ class Screen:
                     count = 0
             final_text = " -> ".join(final_list)
             print(final_text)
-            label_result = Label(self.final_frame, text=final_text, font=('Arial', 20))
-            label_result.pack()
-
+            self.label_result = Label(self.final_frame, text=final_text, font=('Arial', 20))
+            self.label_result.pack()
             back_button = Button(self.final_frame, text='Voltar', bd=4, font=('Arial', 12), width=15, command=self.change_to_menuf)
-            back_button.place(x=10,y=680)
+            back_button.place(x = self.width // 2 - 150 // 2,y = 680)
         show_graph(self.result)
 
     
@@ -86,27 +88,25 @@ class Screen:
         self.final_station_var.set("")
         self.change_frame(self.start_frame, self.final_frame)
         self.run_final_frame()
-        
 
-        
     def run_start_frame(self):
         label_initial_station = Label(self.start_frame, text="Estação Inicial: ", font=('Arial', 12))
-        label_initial_station.place(x = 500, y = 300)
+        label_initial_station.place(x = self.width // 2 - 150 // 2 - 60, y = 300)
 
         entry_initial_station = Entry(self.start_frame, textvariable=self.initial_station_var)
-        entry_initial_station.place(x = 700, y = 300)
+        entry_initial_station.place(x = self.width // 2 - 120 // 2 + 60, y = 300)
 
         label_final_station = Label(self.start_frame, text="Estação Final: ", font=('Arial', 12))
-        label_final_station.place(x = 500, y = 400)
+        label_final_station.place(x = self.width // 2 - 150 // 2 - 60, y = 400)
 
         entry_final_station = Entry(self.start_frame,  textvariable=self.final_station_var)
-        entry_final_station.place(x = 700, y = 400)      
+        entry_final_station.place(x = self.width // 2 - 120 // 2 + 60, y = 400)      
         
         back_button = Button(self.start_frame, text='Voltar', bd=4, font=('Arial', 12), width=15, command=self.change_to_menu)
-        back_button.place(x=10,y=680)
+        back_button.place(x=self.width // 2 - 150 // 2 - 100,y=680)
 
         run_button = Button(self.start_frame, text='Rodar', bd=4, font=('Arial', 12), width=15, command=self.run_dijkstra_algorithm)
-        run_button.place(x=500,y=680)
+        run_button.place(x=self.width // 2 - 150 // 2 + 100,y=680)
         
 
     def run(self):
@@ -119,14 +119,14 @@ class Screen:
         # Imagem do Menu
         image = ImageTk.PhotoImage(Image.open("assets\large_logo.png").resize((400, 200)))
         label = Label(self.menu_frame, image=image, padx=200)
-        label.place(x=450, y=25)
+        label.place(x=self.width // 2 - 400 // 2, y=25)
         
         self.run_menu_frame()
         
         # Imagem da tela do algoritmo
         image2 = ImageTk.PhotoImage(Image.open("assets\large_logo.png").resize((400, 200)))
         label2 = Label(self.start_frame, image=image2)
-        label2.place(x=450, y=25)
+        label2.place(x=self.width // 2 - 400 // 2, y=25)
 
         self.run_start_frame()
 
